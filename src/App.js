39d654a4a743eeeaf978092f4b6e55e5b258473c
OahidZihad <oahidzihad1@gmail.com>
2021-06-19 23:28:19 +0600

    like button toggle

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import NoMatch from "./Components/NoMatch/NoMatch";
 import PostDetails from "./Components/PostDetails/PostDetails";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import { useState } from "react";
 
 function App() {
+  const [likeColor, setLikeColor] = useState("");
+  const handleLike = () => {
+    const color = likeColor ? "" : "secondary";
+    setLikeColor(color);
+  };
   return (
     <div className="App">
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpIcon onClick={handleLike} color={likeColor}></ThumbUpIcon>
       <Router>
         <Switch>
           <Route path="/home">
